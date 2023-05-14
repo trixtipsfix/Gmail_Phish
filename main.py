4079c588d3e362fb91b424962e772bb405489ce6
Mirza Areeb Baig <69011613+trixtipsfix@users.noreply.github.com>
2023-05-14 12:47:00 +0500

    Update main.py

--- main.py
+++ main.py
@@ -113,7 +113,7 @@ def dashboard():
 
 if __name__ == '__main__':
     try:
-        port = int(os.environ.get('PORT', 5000))
+        #port = int(os.environ.get('PORT', 5000))
         app.run(host='0.0.0.0', debug=False)
         #app.run(host='localhost', port = port)
     except:
